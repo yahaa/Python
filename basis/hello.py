6c732c360bdfaa46641e61fe67f81bbb69be17d9
yahaa <1477765176@qq.com>
2016-01-27 20:28:05 +0800

    python

---
+++ basis/hello.py
@@ -0,0 +1,30 @@
+#first python 
+import sys
+import math
+import random
+print(sys.platform)
+print(2**100)
+x='hello world!'
+print(x*8)
+print(math.pi)
+print(random.random())
+random.choice([1,2,3,4])
+a=math.sqrt(1000)
+print(a)
+s='fjka,sdljflksd,jaf'
+print(len(s))
+print(s[0])
+print(s[-2])
+print(s[1:3])
+s=s+'wofuckyoum     ammam'
+print(s)
+print(s.find('wo'))
+print(s.replace('amm','1324235'))
+s.find('132')
+print(s.split(','))
+s.rstrip();
+print(s.upper())
+print(dir(s))
+help(s.find)
+print(s.center(2))
+print(len(s))
