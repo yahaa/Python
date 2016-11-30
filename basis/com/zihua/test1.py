e57f51f324a79ef676cc3bfddb1713817a1cf670
yahaa <1477765176@qq.com>
2016-11-30 19:00:14 +0800

    修改

--- basis/com/zihua/test1.py
+++ basis/com/zihua/test1.py
@@ -1,7 +1,27 @@
-import urllib2
-req =urllib2.Request('http://blog.csdn.net/cqcre')
-try:
-	urllib2.urlopen(req)
-except urllib2.HTTPError ,e:
-	print e.code
-	print e.reason
+for a in [123,35,"jkasd",(1,4,5,6,9),{}]:
+    if a:
+        print a
+
+for a in range(100):
+    if a>10 and a<20 or a>30 and a<40:
+        print a
+b=[1,2,4,5,2,2,10]
+c=[1,2,3]
+b.append(56)
+
+b.append(c)
+print "xxx"
+
+print b
+
+d=(1,2,4)
+e=(5,6,7)
+f={"12":1,"13":6}
+
+for i in f.items():
+    print i
+
+
+
+
+
