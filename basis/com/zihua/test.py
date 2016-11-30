3bbccfe5b86abeda265f6a7ecfbe7f5fe28dbcd6
zihua <1477765176@qq.com>
2016-11-30 17:32:39 +0800

    haha

---
+++ basis/com/zihua/test.py
@@ -0,0 +1,28 @@
+for a in [123,35,"jkasd",(1,4,5,6,9),{}]:
+    if a:
+        print a
+
+for a in range(100):
+    if a>10 and a<20 or a>30 and a<40:
+        print a
+b=[1,2,4,5,2,2,10]
+c=[1,2,3]
+b.append(56)
+
+b.append(c)
+print "xxx"
+
+print b
+
+d=(1,2,4)
+e=(5,6,7)
+f={"12":1,"13":6}
+
+for i in f.items():
+    print i
+
+s="skjsadf"
+
+
+
+
